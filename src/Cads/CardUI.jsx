597ba33bfd9c5a709618cf041265a495db1dd32e
Yashaswini99-tech <yashaswiniraju99@gmail.com>
2020-07-20 23:51:41 +0530

    k

--- src/Cads/CardUI.jsx
+++ src/Cads/CardUI.jsx
@@ -4,8 +4,11 @@ const Card=props=>{
 return(
    <div className="card.text-center shadow">
        <div className="overflow">
+       <div class="card small"  style={{width: '20rem'}}>
         <img src ={props.imgsrc} alt="Image1" className='card-img-top'/>
         </div>
+       
+        
         <div className="card-body text-dark">
             <h4 className="card-title">{props.title}</h4>
             <p className="card-text text-secondary">
@@ -14,7 +17,9 @@ return(
             <div class="text-center">
             <a href="#" class="btn btn-outline-success">USE</a>
             </div>
-        </div>
+            </div>
+            </div>
+        
 </div>
 );
 };
--- src/Cads/Cards.jsx
+++ src/Cads/Cards.jsx
@@ -6,19 +6,22 @@ import img3 from '../assets/im3.jpg';
 class Cards extends Component{
     render(){
     return(
-        <div className='container-fluid.d-flex.justify-content-center'> 
-        <div className='row'>
+        <div className="container-fluid">
+            <div class="d-flex justify-content-around">
+        <div className='row '>
        <div className='col-md-4'>
+           
        <Card imgsrc={img1} title="HARD-WORKER" text="100 points"/>
        </div>
       <div className='col-md-4'>
        <Card imgsrc={img2}title="LEADERSHIP" text="120 points"/>
         </div>
-       <div className='col-md-4'> 
+       <div className='col-md-4'>
        <Card imgsrc={img3}title="TEAM-PLAYER" text="80 points"/>
        </div> 
        </div>
        </div>
+       </div> 
 
     );
 }
